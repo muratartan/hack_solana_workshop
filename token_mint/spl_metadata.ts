44dae4aa64f9e5b4774c6082916bbc8b86c1bd6e
Murat Artan <muratartan@hotmail.com>
2024-04-16 15:28:16 +0300

    comments of metadata

--- token_mint/spl_metadata.ts
+++ token_mint/spl_metadata.ts
@@ -1,3 +1,6 @@
+// metadata account is a part of "token mint account"
+// it stores detailed info about the token
+
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
 import {
   createMetadataAccountV3,
@@ -13,19 +16,31 @@ import {
 
 import wallet from "./wallet.json";
 
+// Define the mint address for the token
 const mint = publicKey("write your mint address here");
 
+// Create an instance of Umi and establish connection to Solana Devnet
 const umi = createUmi("https://api.devnet.solana.com");
-const keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
-const signer = createSignerFromKeypair(umi, keypair);
-umi.use(signerIdentity(createSignerFromKeypair(umi, keypair)));
 
+// Load the keypair from the wallet.json file
+const keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
+
+// Create a signer from the keypair
+const signer = createSignerFromKeypair(umi, keypair);
+
+// Specify the signer identity for Umi
+umi.use(signerIdentity(signer));
+
+// Asynchronous function to create token metadata account
 (async () => {
   try {
+    // Define accounts required for creating metadata account
     let accounts: CreateMetadataAccountV3InstructionAccounts = {
       mint,
       mintAuthority: signer,
     };
+
+    // Define data for the metadata account
     let data: DataV2Args = {
       name: "token name", // write your token name here
       symbol: "SOL", // write your token symbol here
@@ -35,20 +50,29 @@ umi.use(signerIdentity(createSignerFromKeypair(umi, keypair)));
       collection: null,
       uses: null,
     };
+
+    // Define arguments for creating metadata account instruction
     let args: CreateMetadataAccountV3InstructionArgs = {
       data,
       isMutable: true,
       collectionDetails: null,
     };
+
+    // Create the transaction for creating metadata account
     let tx = createMetadataAccountV3(umi, {
       ...accounts,
       ...args,
     });
+
+    // Send and confirm the transaction
     let result = await tx
       .sendAndConfirm(umi)
       .then((r) => r.signature.toString());
+
+    // Log the signature of the transaction
     console.log(result);
   } catch (e) {
+    // Log an error message if something goes wrong
     console.error(`Oops, something went wrong: ${e}`);
   }
 })();
