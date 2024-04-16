1a61858b27c07e26a83bc923914df81580516393
Murat Artan <muratartan@hotmail.com>
2024-04-16 12:07:48 +0300

    comments of decoding

--- token_mint/sk_decode.ts
+++ token_mint/sk_decode.ts
@@ -1,9 +1,11 @@
 import { decode } from "bs58";
 
+// Decode the base58 encoded secret key
 let sk = decode(
   "your secret key" // do not push your secret key to github
 );
 
+// Log the decoded secret key
 console.log(`secret key is: ${sk}`);
 
-// keep your decoded secret key in "wallet.json" and do not push it to Github
+// Keep your decoded secret key in "wallet.json" and do not push it to GitHub
