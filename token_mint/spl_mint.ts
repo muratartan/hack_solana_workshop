1e43acb781cdce2bd1bc1a234e1bfde3efd57019
Murat Artan <muratartan@hotmail.com>
2024-04-16 15:30:26 +0300

    comments of minting to ata

--- token_mint/spl_mint.ts
+++ token_mint/spl_mint.ts
@@ -2,18 +2,25 @@ import { Keypair, PublicKey, Connection, Commitment } from "@solana/web3.js";
 import { getOrCreateAssociatedTokenAccount, mintTo } from "@solana/spl-token";
 import wallet from "./wallet.json";
 
+// Load the keypair from the wallet.json file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
 
+// Specify the commitment level as "confirmed" for transaction confirmation
 const commitment: Commitment = "confirmed";
+
+// Establish a connection to the Solana Devnet with the specified commitment level
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
+// Define the number of decimals for the token
 const token_decimals = 1_000_000n;
 
+// Define the mint address for the token
 const mint = new PublicKey("write your mint address here");
 
+// Asynchronous function to create and mint tokens to an associated token account
 (async () => {
   try {
-    // Create an ATA
+    // Create an Associated Token Account (ATA) for the specified mint and owner
     const ata = await getOrCreateAssociatedTokenAccount(
       connection,
       keypair,
@@ -22,17 +29,18 @@ const mint = new PublicKey("write your mint address here");
     );
     console.log(`Your ata is: ${ata.address.toBase58()}`);
 
-    // Mint to ATA
+    // Mint tokens to the associated token account
     const mintTx = await mintTo(
       connection,
       keypair,
       mint,
       ata.address,
       keypair.publicKey,
-      100n * token_decimals // you can change the token number here, default is 100
+      100n * token_decimals // Specify the number of tokens to mint
     );
     console.log(`Your mint txid: ${mintTx}`);
   } catch (error) {
+    // Log an error message if something goes wrong
     console.log(`Oops, something went wrong: ${error}`);
   }
 })();
