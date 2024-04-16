e7a3e4bb7f9f4c30df04340e6ddb24fcce432918
Murat Artan <muratartan@hotmail.com>
2024-04-16 12:01:41 +0300

    comments

--- token_mint/keygen.ts
+++ token_mint/keygen.ts
@@ -1,6 +1,10 @@
 import { Keypair } from "@solana/web3.js";
 
+// Generate a new keypair using the Keypair class from the Solana Web3.js library
 let kp = Keypair.generate();
 
-console.log(`you generated a new solana wallet: ${kp.publicKey.toBase58()}`);
+// Log the base58 representation of the public key associated with the generated keypair
+console.log(`wallet generated: ${kp.publicKey.toBase58()}`);
+
+// Log the secret key of the generated keypair
 console.log(`secret key is: ${kp.secretKey}`);
