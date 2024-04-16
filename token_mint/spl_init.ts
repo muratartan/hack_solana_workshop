2b9ee9e468a37334228e96738021e5a217001f34
Murat Artan <muratartan@hotmail.com>
2024-04-16 12:09:20 +0300

    comments of token mint

--- token_mint/spl_init.ts
+++ token_mint/spl_init.ts
@@ -2,22 +2,30 @@ import { Keypair, Connection, Commitment } from "@solana/web3.js";
 import { createMint } from "@solana/spl-token";
 import wallet from "./wallet.json";
 
+// Load the keypair from the wallet.json file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
 
+// Specify the commitment level as "confirmed" for transaction confirmation
 const commitment: Commitment = "confirmed";
+
+// Establish a connection to the Solana Devnet with the specified commitment level
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
+// Asynchronous function to create a new token mint account
 (async () => {
   try {
+    // Create a new token mint
     const mint = await createMint(
       connection,
       keypair,
       keypair.publicKey,
       null,
-      6
+      6 // Specify the number of decimal places for the token
     );
+    // Log the address of the created mint
     console.log(`mint address is: ${mint}`);
   } catch (error) {
+    // Log an error message if something goes wrong
     console.log(`Oops, something went wrong: ${error}`);
   }
 })();
